e93641268a9952edd5e67481a0b9f57f476a979d
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-02 14:01:50 +0100

    4.0

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,7 +1,7 @@
 SELECT property_id, name FROM Property
 WHERE property_id IN (
     SELECT property_id FROM Review
-    GROUP BY property_id HAVING AVG(rating) > 4
+    GROUP BY property_id HAVING AVG(rating) > 4.0
 );
 
 SELECT user_id, first_name, last_name FROM Users
