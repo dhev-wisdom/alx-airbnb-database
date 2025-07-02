da496383f1eaf6157bb7bfcaf7dcac45418e9dc7
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-02 14:00:14 +0100

    Advanced SQL Queries - subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,10 @@
+SELECT property_id, name FROM Property
+WHERE property_id IN (
+    SELECT property_id FROM Review
+    GROUP BY property_id HAVING AVG(rating) > 4
+);
+
+SELECT user_id, first_name, last_name FROM Users
+WHERE (
+    SELECT COUNT(*) FROM Booking WHERE Booking.user_id = Users.user_id
+) > 3;
