9af587eda4ff0de11959f3aeb79891cd15232194
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-02 09:18:31 +0100

    SQL JOIN(s)

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,11 @@
+SELECT Booking.booking_id, Users.first_name, Users.last_name
+FROM Booking
+INNER JOIN Users ON Booking.user_id = Users.user_id;
+
+SELECT Property.name, Review.comment
+FROM Property
+LEFT JOIN Review ON Property.property_id = Review.property_id;
+
+SELECT Users.first_name, Users.last_name, Booking.id
+FROM Users
+FULL OUTER JOIN Booking ON Users.user_id = Booking.user_id;
