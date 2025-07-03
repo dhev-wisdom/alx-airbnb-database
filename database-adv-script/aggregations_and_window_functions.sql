55479215a98a7a1fc85ec0127def492fc43b2821
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-03 13:23:29 +0100

    ROW NUMBER()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,8 +3,11 @@ FROM Booking
 JOIN Users ON Users.user_id = Booking.user_id
 GROUP BY Users.user_id, Users.first_name, Users.last_name;
 
-SELECT Property.property_id, Property.name, COUNT(Booking.booking_id) AS number_of_bookings,
-RANK() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS rank
-FROM Booking
-JOIN Property ON Property.property_id = Booking.property_id
-GROUP BY Property.property_id, Property.name;
+SELECT property_id, name, total_bookings,
+ROW NUMBER() OVER (ORDER BY total_bookings DESC) as row_number
+FROM (
+    SELECT Property.property_id, Property.name, COUNT(Booking.booking_id) AS total_bookings
+    FROM Booking
+    JOIN Property ON Booking.property_id = Property.property_id
+    GROUP BY Property.property_id, Property.name
+) AS bookings_count;
