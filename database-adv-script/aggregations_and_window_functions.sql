b09ad19c7e44c0eb223f6d4194a0c068b06da2ba
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-03 13:57:25 +0100

    RANK()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,7 +4,8 @@ JOIN Users ON Users.user_id = Booking.user_id
 GROUP BY Users.user_id, Users.first_name, Users.last_name;
 
 SELECT property_id, name, total_bookings,
-ROW NUMBER() OVER (ORDER BY total_bookings DESC) as row_number
+ROW NUMBER() OVER (ORDER BY total_bookings DESC) as row_number,
+RANK() OVER (ORDER BY total_bookings DESC) AS rank
 FROM (
     SELECT Property.property_id, Property.name, COUNT(Booking.booking_id) AS total_bookings
     FROM Booking
